697fe2a7676a6fdf5fd37af058b88251a65dfe28
刘若愚 <liuruoyu@xunlei.com>
2017-02-16 20:51:49 +0800

    aaa

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"runtime"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	workerNum int
+	releaseMode bool
+	host string
+	port int
+)
+
+func init() {
+	flag.IntVar(&workerNum, "worker", 1, "runtime MAXPROCS value")
+	flag.BoolVar(&releaseMode, "release", false, "gin mode")
+	flag.StringVar(&host, "host", "127.0.0.1", "server host")
+	flag.IntVar(&port, "port", 8090, "server port")
+}
+
+func main() {
+	flag.Parse()
+	address := fmt.Sprintf("%s:%d", host, port)
+	fmt.Printf("%s serving... MAXPROCS:%d release:%t\n", address, workerNum, releaseMode)
+
+	runtime.GOMAXPROCS(workerNum)
+
+	if releaseMode {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	router := gin.Default()
+
+	// add endpoint
+	router.GET("/ping", func(c *gin.Context) {c.String(http.StatusOK, "pong")})
+
+	router.Run(address)
+}
